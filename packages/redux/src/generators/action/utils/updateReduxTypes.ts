6aa8a5d9ba81f8e2e6fbf5f4af96d9210093211f
James Grom <jgrom@scu.edu>
2022-05-28 13:40:56 -0700

    cursory refactor to update index.ts for redux types

---
+++ packages/redux/src/generators/action/utils/updateReduxTypes.ts
@@ -0,0 +1,67 @@
+import { readFileIfExisting } from 'nx/src/project-graph/file-utils';
+import { Tree } from '@nrwl/devkit';
+import * as path from 'path';
+import { NormalizedSchema } from '../generator';
+
+export const updateReduxTypes = (tree: Tree, options: NormalizedSchema) => {
+  const targetFilePath = path.join(options.projectRoot, '/src/index.ts');
+  let content = readFileIfExisting(targetFilePath);
+  content = addNewAction(content, options.actionName);
+  if (options.includesLoader)
+    content = addNewAction(content, `${options.actionName}_Loading`);
+  if (options.includesError)
+    content = addNewAction(content, `${options.actionName}_Error`);
+  tree.write(targetFilePath, content);
+};
+const addNewAction = (content: string, actionName: string): string => {
+  content = importActionInterface(content, actionName);
+  content = importReducer(content, actionName);
+  content = updateActionTypesEnum(content, actionName);
+  content = updateAction(content, actionName);
+  return content;
+};
+
+const updateActionTypesEnum = (content: string, actionName: string): string => {
+  const replacementAnchorString = 'export enum ACTION_TYPES {';
+  const newContent = `\n${actionName.toUpperCase()}="${actionName.toUpperCase()}",`;
+  content = content.replace(
+    replacementAnchorString,
+    `${replacementAnchorString}${newContent}`
+  );
+  return content;
+};
+const updateAction = (content: string, actionName: string): string => {
+  const replacementAnchorString = 'export type Action =';
+  const newContent = ` ${actionName}Interface |`;
+  content = content.replace(
+    replacementAnchorString,
+    `${replacementAnchorString}${newContent}`
+  );
+  return content;
+};
+const importActionInterface = (content: string, actionName: string): string => {
+  const replacementAnchorString = 'ACTION--INTERFACE--IMPORTS';
+  const newContent = `\nimport {${actionName}Interface} from "./${actionName}";`;
+  content = content.replace(
+    replacementAnchorString,
+    `${replacementAnchorString}${newContent}`
+  );
+  return content;
+};
+const importReducer = (content: string, actionName: string): string => {
+  const replacementAnchorString = 'REDUCER--IMPORTS';
+  const newContent = `\nimport {${actionName}Reducer} from "./${actionName}";`;
+  content = content.replace(
+    replacementAnchorString,
+    `${replacementAnchorString}${newContent}`
+  );
+  return content;
+};
+
+// const updateContent = (
+//   content: string,
+//   replacementAnchorString: string,
+//   newContent: string
+// ): string => {
+//   return content;
+// };
